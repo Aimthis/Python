22c61ebc2275e6e0ed5740c553664b19b9d65036
aimthis <aimthis@mail.ru>
2023-08-28 09:53:41 +0500

    done dz24 28.08.2023

--- 24082023/DZ24.py
+++ 24082023/DZ24.py
@@ -11,3 +11,16 @@
 # Напишите программу для нахождения максимального числа ягод, которое может
 # собрать за один заход собирающий модуль, находясь перед некоторым кустом
 # заданной во входном файле грядки.
+
+from random import randint
+n = int(input('Введите количество кустов: '))
+arr = list()
+for i in range(n):
+    a = randint(1, 20)
+    arr.append(a)
+print(f'На кустах созрело следующее количество ягод {arr}')
+arr_count = list()
+for i in range(len(arr) - 1):
+    arr_count.append(arr[i - 1] + arr[i] + arr[i + 1])
+arr_count.append(arr[-2] + arr[-1] + arr[0])
+print(f'Собирающий модуль за один заход собирет следующее максимальное количество ягод : {max(arr_count)}')
