c5b172b60ddfba1beca87d8725c47975aff18277
aimthis <aimthis@mail.ru>
2023-09-16 18:06:26 +0500

    point 18:06 DZ38 16092023

--- 16092023/phone_book.py
+++ 16092023/phone_book.py
@@ -3,10 +3,14 @@
 # Программа должна сохранять данные в текстовом файле
 # Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека)
 # Использование функций. Ваша программа не должна быть линейной
-import os
-import shutil
-clear = lambda: os.system('cls')
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал
+# для изменения и удаления данных.
+import os, shutil, subprocess
+
+clear = lambda: os.system("cls")
 clear()
+
 
 def Vvod_imeni():
     return input("Введите имя: ").capitalize()
@@ -27,19 +31,46 @@ def Vvod_telephona():
 def Vvod_adressa():
     return input("Введите адрес: ").capitalize()
 
+
 def input_data():
     surname = Vvod_familii()
     name = Vvod_imeni()
     patronymic = Vvod_otchestva()
     phone = Vvod_telephona()
     address = Vvod_adressa()
+    string_of_data = f"{surname} {name} {patronymic}: {phone} \n {address}\n\n"
+    return string_of_data
 
+def adding_to_pb():
     with open("phone_book.csv", "a", encoding="utf-8") as file:
-        file.write(f"{surname} {name} {patronymic}: {phone} \n {address}\n\n")
+        file.write(input_data())
+
 
 def print_data():
     with open("phone_book.csv", "r", encoding="utf-8") as file:
         print(file.read())
+
+
+def edit_data():
+    clear()
+    print("Введите в поиск заменяемые данные")
+    old_data = search_line()
+    a = 0
+    while a != 1 or a !=5:
+        print(old_data)
+        print('Для продолжения нажмите 1, для отмены 5')
+        a = input('Хотите изменить эти данные?')
+    if a == 5: return
+    print("Введите новые данные")
+    new_data = input_data()
+    print(new_data)
+    a = input('')
+    with open("phone_book.csv", "r", encoding="utf-8") as file:
+        old_total_data = file
+    new_total_data = old_total_data.writelines(f'{old_data}', f'{new_data}', 1)
+    with open ('test.txt', 'w') as file:
+        file.write(new_total_data)
+
 
 def search_line():
     search = input("Введите данные для поиска: ").capitalize()
@@ -47,32 +78,41 @@ def search_line():
         data = file.read().split("\n\n")[:-1]
         for line in data:
             if search in line:
-                print(line + '\n')
+                return line
+
 
 def user_interface():
     choice = 0
-    while choice != 4:
+    while choice != 5:
         clear()
-        print('''Выберите действие:
+        print(
+            """Выберите действие:
                     1 - Запись данных
                     2 - Вывод данных
                     3 - Поиск данных
-                    4 - Выход''')
-        choice = int(input('номер операции: '))
-        match(choice):
+                    4 - Изменение данных
+                    5 - Выход"""
+        )
+        choice = int(input("номер операции: "))
+        match (choice):
             case 1:
                 clear()
-                input_data()
-                choice = input('Данные внесены. Для продолжения нажмите любую кнопку.')
-            case 2: 
+                adding_to_pb()
+                choice = input("Данные внесены. Для продолжения нажмите любую кнопку.")
+            case 2:
                 clear()
                 print_data()
-                choice = input('Для продолжения нажмите любую кнопку.')
-            case 3: 
+                choice = input("Для продолжения нажмите любую кнопку.")
+            case 3:
                 clear()
-                search_line()
-                choice = input('Для продолжения нажмите любую кнопку.')
+                print(search_line() + "\n")
+                choice = input("Для продолжения нажмите любую кнопку.")
             case 4:
                 clear()
-                print('Досвидания.')
+                edit_data()
+            case 5:
+                clear()
+                print("Досвидания.")
+
+
 user_interface()
