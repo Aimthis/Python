fe70c741758255b9d9141b397aa0134c44f55ce8
aimthis <aimthis@mail.ru>
2023-09-16 14:48:22 +0500

    point 14:48 DZ38 16092023

--- 16092023/phone_book.py
+++ 16092023/phone_book.py
@@ -3,6 +3,8 @@
 # Программа должна сохранять данные в текстовом файле
 # Пользователь может ввести одну из характеристик для поиска определенной записи(Например имя или фамилию человека)
 # Использование функций. Ваша программа не должна быть линейной
+import os
+import shutil
 def Vvod_imeni ():
     return input('Введите имя: ').capitalize
 def Vvod_familii ():
@@ -13,7 +15,7 @@ def Vvod_telephona ():
     return input('Введите номер телефона: ').capitalize
 def Vvod_adressa ():
     return input('Введите номер адрес: ').capitalize
-def Vvod_dannyh ():
+def input_data ():
     name = Vvod_imeni
     patronymic = Vvod_otchestva
     surname = Vvod_familii
@@ -24,5 +26,17 @@ def Vvod_dannyh ():
         print(file)
         file.write(f'{name} {patronymic} {surname}: {phone} \n {address}\n\n')
 
-def search ():
-    with open
+def print_data():
+    with open('phone_book.csv', 'r', encoding='utf-8') as file:
+        print(file.read())
+
+def search_line ():
+    search = input('Введите данные для поиска: ').capitalize
+    with open('phone_book.csv', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n\n')[:-1]
+        print(data)
+        for line in data:
+            if search in line:
+                print(line)
+
+input_data()
